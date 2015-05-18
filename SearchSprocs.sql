1cbe9c15bed2361ebe0511f7df69366db8068866
bonattt <bonattt@rose-hulman.edu>
2015-05-18 17:16:02 -0400

    committed search sprocs

--- SearchSprocs.sql
+++ SearchSprocs.sql
@@ -10,7 +10,7 @@ GO
 CREATE PROCEDURE SearchWholeLocation
 @location varchar(45)
 AS
-	SELECT fname + ' ' + midint + ' ' + lname, Location.name AS 'location', Location.[type]
+	SELECT fname + ' ' + midint + ' ' + lname AS [leader], Location.name AS [location], Location.[type]
 	FROM (
 		Leader INNER JOIN Leads ON Leader.leader_id = Leads.leader_id
 		INNER JOIN Location ON Location.ppID = Leads.locationID
@@ -19,4 +19,49 @@ AS
 	WHERE location.name = '%' + @location +'%'
 	-- TODO ask Dr. Kulter about need for loops here. --
 GO
+
+CREATE PROCEDURE DeleteRating
+@username varchar(10)
+@leader_id smallint
 AS
+	IF NOT EXISTS (
+			SELECT *
+			FROM User_Account
+			WHERE [username] = @username
+		)
+		BEGIN
+		PRINT('That user does not exist')
+		END
+
+	ELSE IF NOT EXISTS (
+			SELECT *
+			FROM User_Account
+			WHERE [username] = @username
+		)
+		BEGIN
+		PRINT('That leader does not exist')
+		END
+
+	ELSE IF NOT EXISTS (
+			SELECT *
+			FROM Rating
+			WHERE [username] = @username AND [leader_id] = @leader_id
+		)
+		BEGIN
+		PRINT('That Rating Does not exist')
+		END
+
+	ELSE BEGIN
+		DROP FROM Rating
+		WHERE [username] = @username AND [leader_id] = @leader_id
+
+		UPDATE User_Accound
+		SET number_ratings = number_ratings - 1
+		WHERE [username] = @username
+		END
+GO
+
+/*CREATE PROCEDURE SearchUser
+@username varchar(10)
+AS
+	SELECT fname + ' ' + midint + ' ' + lname AS [leader], */
